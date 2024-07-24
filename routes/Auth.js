feed0fd4b9a28403155d04d1e861556e2e3acb16
Piyush12800 <12800piyush@gmail.com>
2024-07-24 12:16:37 +0530

    Add reset and update password

--- routes/Auth.js
+++ routes/Auth.js
@@ -2,8 +2,16 @@ const express = require('express')
 const router = express.Router()
 const {auth} = require('../middlewares/auth')
 const { signup , login, sendotp,changePassword } = require('../controllers/Auth')
+const {resetPassword,resetPasswordToken} = require('../controllers/ResetPassword')
 router.post('/signup',signup)
 router.post('/login',login)
 router.post('/sendotp',sendotp)
 router.post('/changePassword',auth,changePassword)
+
+
+// Route for generating a reset password token
+router.post("/reset-password-token", resetPasswordToken)
+
+// Route for resetting user's password after verification
+router.post("/reset-password", resetPassword)
 module.exports=router; 
