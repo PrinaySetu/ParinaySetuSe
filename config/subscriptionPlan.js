2930cb607cd98105bb2e9f462ceaaaed41aa0cca
PrinaySetu <parinaysetu2701@gmail.com>
2024-11-15 09:01:01 +0530

    Update subscriptionPlan.js

--- config/subscriptionPlan.js
+++ config/subscriptionPlan.js
@@ -2,21 +2,21 @@ module.exports = [
     {
         id: 'basic-annual',
         name: 'Basic Annual',
-        price: 1,
+        price: 3100,
         duration: 365, // days
         features: ['Information about the biodata of the other party will be made available for 1 year or till the marriage, whichever is earlier.']
     },
     {
         id: 'premium-annual',
         name: 'Premium Annual',
-        price: 2,
+        price: 5100,
         duration: 730, // days
         features: ['Verified information of the biodata of the other party will be provided for 2 years or till the marriage, whichever is earlier.']
     },
     {
         id: 'lifetime',
         name: 'Lifetime',
-        price: 3,
+        price: 7100,
         duration: 730, // null indicates lifetime
         features: [' Verified information about the biodata of the other party will be given.', 
             'Information will be provided for 2 years or till marriage, whichever is earlier.',
@@ -25,7 +25,7 @@ module.exports = [
     {
         id:'lifetime-premium',
         name:'Lifetime Premium',
-        price: 4,
+        price: 11000,
         duration: null,
         features:['Verified information about the biodata of the other party will be given',
             'This information will be made available till the marriage takes place.'
